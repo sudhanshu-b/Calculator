d4aef41ccd806b40595c7ff175e8fd5853332b3c
Sudhanshu Burile <123616951+sudhanshu-b@users.noreply.github.com>
2023-02-11 13:42:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,21 @@ Array.from(buttons).forEach((button)=>{
         if(e.target.innerHTML=='='){
              string=eval(string);
              document.querySelector('input').value=string;
+             var beep=new Audio('beep.mp3')
+             beep.play();
         }
         else if(e.target.innerHTML=='AC'){
             string="";
             document.querySelector('input').value=string;
+            var sound=new Audio('click-sound.mp3')
+            sound.play();
         }
         else{
             console.log(e.target);
             string=string+e.target.innerHTML;
             document.querySelector('input').value=string;
+            var sound=new Audio('click-sound.mp3')
+            sound.play();
         }
        
     })
